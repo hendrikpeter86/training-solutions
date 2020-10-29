74563dc8f770ad8abcabbf9a8aa62a2e3a793b9a
hendrikpeter <hendrikpeter@gmail.com>
2020-10-29 17:10:29 +0100

    probal feltoltes, hova tolti fel, hol jelenik meg ez a Commit Message?

--- src/main/java/classstructureio/Calculator.java
+++ src/main/java/classstructureio/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Kérek egy sámot: ");
+        System.out.println("Kérek egy számot: ");
         int egyikSzam = scanner.nextInt();
 
         System.out.println("Kérek még egy számot: ");
