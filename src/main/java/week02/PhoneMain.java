f9961c94e9997686f32bc8e6ad33037574484c77
hendrikpeter <hendrikpeter@gmail.com>
2020-11-03 10:17:03 +0100

    egyszerre mennek többen

---
+++ src/main/java/week02/PhoneMain.java
@@ -0,0 +1,9 @@
+package week02;
+
+public class PhoneMain {
+
+    public static void main(String[] args) {
+        Phone phone1 = new Phone("apple", 8);
+        Phone phone2 = new Phone("Samsung" 16);
+    }
+}
