9ec1281f2c3b0633df7c3c8e279d129029ea16e5
hendrikpeter <hendrikpeter@gmail.com>
2020-11-03 10:17:41 +0100

    javítás egy vessző hiányzott

--- src/main/java/week02/PhoneMain.java
+++ src/main/java/week02/PhoneMain.java
@@ -4,6 +4,6 @@ public class PhoneMain {
 
     public static void main(String[] args) {
         Phone phone1 = new Phone("apple", 8);
-        Phone phone2 = new Phone("Samsung" 16);
+        Phone phone2 = new Phone("Samsung", 16);
     }
 }
