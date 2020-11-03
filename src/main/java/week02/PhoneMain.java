3842e586cbfcc4be7d82107aa8d74dcf3e532b4c
hendrikpeter <hendrikpeter@gmail.com>
2020-11-03 10:19:11 +0100

    kieg

--- src/main/java/week02/PhoneMain.java
+++ src/main/java/week02/PhoneMain.java
@@ -5,5 +5,10 @@ public class PhoneMain {
     public static void main(String[] args) {
         Phone phone1 = new Phone("apple", 8);
         Phone phone2 = new Phone("Samsung", 16);
+
+        System.out.println(phone1.type + ", " + phone1.mem);
+        System.out.println(phone2.type + ", " + phone2.mem);
     }
+
+
 }
