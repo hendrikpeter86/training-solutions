3f3a6244f72916628a07dd9e70c2afbe72d65dcc
hendrikpeter <hendrikpeter@gmail.com>
2020-11-04 19:39:11 +0100

    több egyszerre, így minek ez a szöveg?

---
+++ src/main/java/aAndKAkademia/forCiklus.java
@@ -0,0 +1,21 @@
+package aAndKAkademia;
+
+public class forCiklus {
+    public static void main(String[] args) {
+
+   /*     System.out.println("Hello World!"); //ha szeretnénk megváltoztatni pl ! nélkül, akkor sokszor kell átírni
+        System.out.println("Hello World!"); //azért is kell for ciklus, mert sokszor nem tudjuk előre hányszor szeretnénk, mert a felhasználó adja meg
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");*/
+
+
+        for (int i = 0; i < 5; i++) {       //for (;;) {}
+            System.out.println("Hello World!");  //első kifejezés csak egyszer fut le mindig: int i = 0
+                                                //második kifejezés boleean kell lennie, ha igaz, akkor megy tovább a harmadikra
+
+        }
+
+
+    }
+}
---
+++ src/main/java/aAndKAkademia/forEachCiklus.java
@@ -0,0 +1,39 @@
+package aAndKAkademia;
+
+public class forEachCiklus {
+
+    public static void main(String[] args) {
+
+        String[] names = {"Teszt Elek", "John Doe", "Jane Doe"};
+
+        for (String name : names) {    //for (:){}    :-tól jobbra valamilyen tömb egyenlőre ,később tanulunk mást is
+            System.out.println("Név: " + name);     //for each, tehát mindegyikre egyszer hajtja végre, végig megy a tömb összes elemén
+        }
+
+
+
+        String[] fruits = {"alma", "körte", "szilva"};
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+
+
+        int[] winningLotteryNumbers = {43, 66, 10, 11, 42};
+        for (int winningNubmer : winningLotteryNumbers) {
+            System.out.println(winningNubmer);
+        }
+
+        boolean[] answers = { true, true, false};
+        for (boolean answer : answers){
+            System.out.println(answer);
+        }
+
+        for (int i = 0; i < answers.length; i++) {  //ez ugyanazt csinálja, mint az eggyel felette lévő for each ciklus
+            System.out.println(answers[i]);
+        }
+
+    }
+
+}
+
