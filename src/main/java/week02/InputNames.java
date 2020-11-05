5cab0c3e0a06d7c16b92fb9a29f846eb022bdcba
hendrikpeter <hendrikpeter@gmail.com>
2020-11-05 10:34:26 +0100

    névbekérős javított

--- src/main/java/week02/InputNames.java
+++ src/main/java/week02/InputNames.java
@@ -6,10 +6,10 @@ public class InputNames {
 
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         String[] names = new String[5];
-        for (int i = 1; i <= 5 ; i++ ) {
-            Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 5 ; i++ ) {
             System.out.println("Kérem adjon meg egy nevet: ");
             names[i] = scanner.nextLine();
 
