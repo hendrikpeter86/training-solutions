d1ceb20972ed65c7f974fe8e547fbffdb2b0b5a3
hendrikpeter <hendrikpeter@gmail.com>
2020-11-26 11:21:03 +0100

    ...

---
+++ src/main/java/SanFranciscobolJottem/ElsoProject.java
@@ -0,0 +1,16 @@
+package SanFranciscobolJottem;
+
+public class ElsoProject {
+
+    public static void main(String[] args) {
+
+    }
+
+    void MEtodus (){
+
+    }
+
+    String Fuggveny (){
+        return null;
+    }
+}
