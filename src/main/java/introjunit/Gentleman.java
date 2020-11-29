3dd72c042eaf69cf60249f422a4199b85aeb39c9
hendrikpeter <hendrikpeter@gmail.com>
2020-11-29 13:20:28 +0100

    második önálló sikeres tesztem :)

--- src/main/java/introjunit/Gentleman.java
+++ src/main/java/introjunit/Gentleman.java
@@ -3,7 +3,20 @@ package introjunit;
 public class Gentleman {
 
    public String sayHello(String name){
-       return "Hello " + name;
+       if (name == null){
+           return "Hello Anonymus";
+       }else{
+           return "Hello " + name;
+       }
+
    }
+/*
+
+    public static void main(String[] args) {
+        Gentleman gentleman = new Gentleman();
+        String koszones = gentleman.sayHello(null);
+        System.out.println(koszones);
+    }
+*/
 
 }
--- src/test/java/introjunit/GentlemanTest.java
+++ src/test/java/introjunit/GentlemanTest.java
@@ -7,9 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GentlemanTest {
 
     @Test
-    public void testSayHello(){
-    Gentleman gentleman = new Gentleman();
-    String koszones = gentleman.sayHello("Peti");
-    assertEquals ("Hello Peti", koszones);
-        }
+    public void testSayHello() {
+        Gentleman gentleman = new Gentleman();
+        String koszones = gentleman.sayHello("Peti");
+        assertEquals("Hello Peti", koszones);
     }
+
+
+    @Test
+    public void testSayHelloAnonymus(){
+        Gentleman gentleman = new Gentleman();
+        String koszones = gentleman.sayHello(null);
+        assertEquals("Hello Anonymus", koszones);
+    }
+}
