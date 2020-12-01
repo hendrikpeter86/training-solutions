09342dafdd8af32cd13de474a9f0b25ae74257ed
hendrikpeter <hendrikpeter@gmail.com>
2020-12-01 09:45:25 +0100

    itt is csak az alapok mennek, metódust nem is értem mit kéne csinálnia

---
+++ src/main/java/week06d02/Product.java
@@ -0,0 +1,9 @@
+package week06d02;
+
+public class Product {
+    private String name;
+    private String category;
+    private int price;
+
+
+}
---
+++ src/main/java/week06d02/Store.java
@@ -0,0 +1,17 @@
+package week06d02;
+
+import java.util.List;
+
+public class Store {
+
+    List<String> Product;
+
+    public Store(List<String> product) {
+        Product = product;
+    }
+
+/*    getProductByCategoryName(Category){
+        return;
+    }*/
+
+}
