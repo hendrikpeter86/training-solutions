fe0d623a102a934dfca8253ee09105d844896b0d
hendrikpeter <hendrikpeter@gmail.com>
2020-12-06 17:19:00 +0100

    idáig eljutottam...

---
+++ src/main/java/week06d05/Biscuit.java
@@ -0,0 +1,14 @@
+package week06d05;
+
+public class Biscuit {
+    int gramAmount;
+   BiscuitType type;
+
+
+        public void of(int gramAmount, BiscuitType type) {
+            this.gramAmount = gramAmount;
+            this.type = type;
+        }
+
+
+}
