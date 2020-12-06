e4020d6693cf849657b02f65791bd18dde7055d4
hendrikpeter <hendrikpeter@gmail.com>
2020-12-06 17:18:40 +0100

    javítva a week mappa neve, amúgy hozzá sem tudok szagolni

--- src/main/java/week06d04/Budget.java
+++ src/main/java/week06d04/Budget.java
@@ -1,4 +1,4 @@
-package weed06d04;
+package week06d04;
 
 public class Budget {
     //hát ezt sem értem, bemásolni meg tök felesleges lenne, majd hónapok múlva visszatérek rá, ha már meglesz a tudásom hozzá
