ed64ac311884dbcde4c4d7ddf21f92db8c1e8c8f
hendrikpeter <hendrikpeter@gmail.com>
2020-12-14 00:14:10 +0100

    Számsorrend ellenőrző teszttel

---
+++ src/test/java/week07d04/isIncreasingTest.java
@@ -0,0 +1,22 @@
+package week07d04;
+
+import org.junit.jupiter.api.Test;
+import week07d03.NumberList;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class isIncreasingTest {
+
+
+    @Test
+    public void isIncreasingTest(){
+        NumberList nl = new NumberList();
+
+        assertTrue(nl.isIncreasing(Arrays.asList(-1,1,2,4,5)));
+        assertTrue(nl.isIncreasing(Arrays.asList(-100,1,20,40,556)));
+        assertFalse(nl.isIncreasing(Arrays.asList(1,2,9,5)));
+        assertFalse(nl.isIncreasing(Arrays.asList(1,2,-4,5)));
+    }
+}
