51f118f26542ab89f822a2e98507b033a0f78da4
hendrikpeter <hendrikpeter@gmail.com>
2020-12-13 09:24:46 +0100

    equals gyakorlása

--- src/main/java/week02/Product.java
+++ src/main/java/week02/Product.java
@@ -4,34 +4,29 @@ public class Product {
     private String name;
     private String code;
 
-    public String getName() {
-        return name;
+    public Product(String name, String code) {
+        this.name = name;
+        this.code = code;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getName() {
+        return name;
     }
 
     public String getCode() {
         return code;
     }
 
-    public void setCode(String code) {
-        this.code = code;
+    public boolean areTheyEqual(Product p){
+        return name.equals(p.name);
     }
-
-    public Product(String name, String code) {
-        this.name = name;
-        this.code = code;
-    }
-
-/*    public boolean areTheyEqual(Product p) {
-        return p.name
-    }*/
 
     public static void main(String[] args) {
-        Product product1 = new Product("Samsung", "1");
-        Product product2 = new Product("Nokia", "2");
+        Product product1 = new Product("termek1", "111");
+        Product product2 = new Product("termek1", "112");
+        Product product3 = new Product("termek3", "113");
 
-        System.out.println(product1);}
+        System.out.println(product1.areTheyEqual(product2));
+        System.out.println(product1.areTheyEqual(product3));
+    }
 }
