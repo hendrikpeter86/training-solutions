8fbaaa0f28401d411ac2bc10b31592723d410464
hendrikpeter <hendrikpeter@gmail.com>
2020-12-16 13:29:29 +0100

    BodyMassTest 2.0 saját fejlesztés :)

--- src/test/java/methodstructure/BodyMassTest.java
+++ src/test/java/methodstructure/BodyMassTest.java
@@ -9,9 +9,9 @@ public class BodyMassTest {
 
     @Test
     public void bodyMassIndex(){
-    BodyMass bmOver = new BodyMass(60,1.8);
-    BodyMass bmUnder = new BodyMass(110,1.8);
-    BodyMass bmNormal = new BodyMass(70,1.8);
+    BodyMass bmOver = new BodyMass(120,1.8);
+    BodyMass bmUnder = new BodyMass(40,1.8);
+    BodyMass bmNormal = new BodyMass(60,1.8);
 
     assertEquals(BmiCategory.OVERWEIGHT, bmOver.body());
     assertEquals(BmiCategory.UNDERWEIGHT, bmUnder.body());
