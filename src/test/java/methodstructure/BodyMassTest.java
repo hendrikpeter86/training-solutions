f5b25a2851ba9cc917057de91e51b1d1f9e7736b
hendrikpeter <hendrikpeter@gmail.com>
2020-12-16 13:28:03 +0100

    BodyMassTest saját fejlesztés :)

---
+++ src/test/java/methodstructure/BodyMassTest.java
@@ -0,0 +1,21 @@
+package methodstructure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class BodyMassTest {
+
+    @Test
+    public void bodyMassIndex(){
+    BodyMass bmOver = new BodyMass(60,1.8);
+    BodyMass bmUnder = new BodyMass(110,1.8);
+    BodyMass bmNormal = new BodyMass(70,1.8);
+
+    assertEquals(BmiCategory.OVERWEIGHT, bmOver.body());
+    assertEquals(BmiCategory.UNDERWEIGHT, bmUnder.body());
+    assertEquals(BmiCategory.NORMAL, bmNormal.body());
+    }
+
+}
