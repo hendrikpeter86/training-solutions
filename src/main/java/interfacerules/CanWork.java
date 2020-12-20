20621a082ba9e8e0f33d7464165d2049156302ed
hendrikpeter <hendrikpeter@gmail.com>
2020-12-20 21:14:11 +0100

    interfész szabályok

---
+++ src/main/java/interfacerules/CanWork.java
@@ -0,0 +1,6 @@
+package interfacerules;
+
+public interface CanWork {
+
+    void doWork();  //ha nem adok meg módosító szót, alapból abstract és public kap
+}
---
+++ src/main/java/interfacerules/EmployeeType.java
@@ -0,0 +1,7 @@
+package interfacerules;
+
+public interface EmployeeType {
+
+    int FULL_TIME = 0;  //interfaceben alapból public static final, tehát konstans, ezért nagy betű
+    int PART_TIME = 1;
+}
