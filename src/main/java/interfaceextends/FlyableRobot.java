7ec9f71c600ae1cac7853a3b64e34f11bfcb57a5
hendrikpeter <hendrikpeter@gmail.com>
2020-12-21 20:07:41 +0100

    interface öröklődés, C3PO

---
+++ src/main/java/interfaceextends/FlyableRobot.java
@@ -0,0 +1,6 @@
+package interfaceextends;
+
+public interface FlyableRobot extends MovableRobot{
+
+    void liftTo(long altitude);
+}
---
+++ src/main/java/interfaceextends/MovableRobot.java
@@ -0,0 +1,14 @@
+package interfaceextends;
+
+import java.util.List;
+
+public interface MovableRobot {
+
+    void moveTo(Point position);
+
+    void fastMoveTo(Point position);
+
+    void rotate(int angle);
+
+    List<Point> getPath();
+}
