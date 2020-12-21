4f5a14de148ff0698de8521c2809bb4aa284effc
hendrikpeter <hendrikpeter@gmail.com>
2020-12-21 20:31:19 +0100

    week08d01 Robot

---
+++ src/test/java/week08d01/RobotTest.java
@@ -0,0 +1,25 @@
+package week08d01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RobotTest {
+
+    Position position = new Position(0, 0);
+    Robot robot = new Robot();
+
+    @Test
+    public void testPosition() {
+        robot.dirrection(position, "FFLLLLLBBBBJJJJJJJ");
+        assertEquals(3, position.getX());
+        assertEquals(-3, position.getY());
+    }
+
+    @Test
+    public void testPositionError() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> robot.dirrection(position, "FFLALLLBBBBJJJJJJJ"));
+        assertEquals("Hibás utasítás!", ex.getMessage());
+    }
+
+}
