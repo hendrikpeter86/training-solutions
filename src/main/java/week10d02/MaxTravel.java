81b3dedab375b75020f61b7f5e0e69db196bf10b
hendrikpeter <hendrikpeter@gmail.com>
2021-01-05 13:23:17 +0100

    Buszmegállós feladat

--- src/main/java/week10d02/MaxTravel.java
+++ src/main/java/week10d02/MaxTravel.java
@@ -2,6 +2,8 @@ package week10d02;
 
 public class MaxTravel {
 
+    int[] stations = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29};
+
 /*    public String getMaxIndex(){
         return;
     }*/
