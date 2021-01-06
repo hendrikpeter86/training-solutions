e2a8faf00f17cfd3701999fbf82a5a58696598b2
hendrikpeter <hendrikpeter@gmail.com>
2021-01-06 19:37:25 +0100

    2. részvizsga Cv feladat

---
+++ src/main/java/exam02/cv/Cv.java
@@ -0,0 +1,24 @@
+package exam02.cv;
+
+public class Cv {
+
+    private String name;
+    private Skill skills;
+
+    public Cv(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Skill getSkills() {
+        return skills;
+    }
+
+    public String addSkills (){
+        return "something";
+
+    }
+}
